0e0a53b9ab2bff4e1e19402400ad7cbdd5a3f132
SaajanBhatia <Saajan@ssbhatia.co.uk>
2021-11-12 19:23:48 +0000

    electron dev deleted

--- public/electron.js
+++ public/electron.js
@@ -1,7 +1,7 @@
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-
+ 
 const path = require('path');
 const url = require('url');
 
--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
   return (
     <div className="App">
       <Login />
-
+ 
 
       <Router>
         <SideMenu
